a3e5ea83ed297c4f0e4528b996da560c111112d1
kamrulhaider <kamrulhaider.ssb@gmail.com>
2022-02-07 21:21:45 +0600

    add doctor

--- js/count.js
+++ js/count.js
@@ -82,4 +82,46 @@ minusDraw.addEventListener('click', function (event) {
     }
 });
 
-document.getElementById('txtDate').min = new Date().toISOString().substring(0, 10);
+
+
+// const someCheck = new Date().toISOString().split("T");
+// let newDate = someCheck[0].substring(8, 10);
+// let newTime = new Date().getUTCDate();
+// let finalDate;
+// if (parseInt(newTime) >= 18 && parseInt(newTime) <= 10) {
+//     const dateParse = "0" + (parseInt(newDate) + 1);
+//     finalDate = new Date().toISOString().substring(0, 8) + dateParse;
+// } else {
+//     finalDate = new Date().toISOString().substring(0, 10);
+// }
+
+// console.log(newTime)
+// document.getElementById('txtDate').min = finalDate;
+
+// document.getElementById('txtDate').min = ;
+
+const date = new Date().getDate();
+let dateVal;
+if (date < 10) {
+    dateVal = "0" + date;
+} else {
+    dateVal = date;
+}
+const month = new Date().getMonth() + 1;
+let monthVal;
+if (month < 10) {
+    monthVal = "0" + month;
+} else {
+    monthVal = month;
+}
+const year = new Date().getFullYear();
+const time = new Date().getHours();
+let finalDate;
+if (time >= 16) {
+    finalDate = year + "-" + monthVal + "-" + dateVal;
+} else {
+    finalDate = new Date().toISOString().substring(0, 10);
+}
+
+document.getElementById('txtDate').min = finalDate;
+console.log(finalDate, dateVal)
