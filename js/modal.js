9bd9bb3e9837e2492c36a71d4b6439b37c143f7b
kamrulhaider <kamrulhaider.ssb@gmail.com>
2022-02-11 00:53:59 +0600

    add volunteer

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,14 @@ for (let i = 0; i < editIcon.length; i++) {
     count = i;
   })
 };
+function clearInput() {
+  document.getElementById("docCategoryNameEdit").value = "";
+  const successMsg = document.getElementById("success-text").removeAttribute("hidden", true);
+}
 
 document.getElementById("changeCategoryName").addEventListener("click", function () {
   const newInput = document.getElementById("docCategoryNameEdit").value;
   categoryExistingName[count].innerHTML = newInput;
+  clearInput()
 }
 );
